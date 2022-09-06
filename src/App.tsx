bf77acb2fbc63e867d862113716204c08eb499a0
migmi69 <legolas.greenleaf15812@gmail.com>
2022-09-06 14:52:06 +0430

    after video

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { BeforeAfter } from "./Components";
 
 function App() {
   return (
-    <div className="App">
-      <div className="w-[80vw] h-[80vh] mx-auto my-4">
-        <BeforeAfter
-          before={{ src: "./images/before.png" }}
-          after={{ src: "./images/after.png" }}
-        />
-      </div>
+    <div className="w-[40vw] h-[60vh] mx-auto my-4">
+      <BeforeAfter
+        before={{ src: "./images/before1.png" }}
+        after={{ src: "./images/after1.png" }}
+      />
     </div>
   );
 }
--- src/Components/BeforeAfter/BeforeAfter.tsx
+++ src/Components/BeforeAfter/BeforeAfter.tsx
@@ -1,19 +1,17 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useEffect } from "react";
-import { useRef } from "react";
-import { useState } from "react";
-import { BeforeAfterProps } from "./type";
+import { useEffect, useRef, useState } from "react";
+import { BeforeAfterProps } from "./types";
 
-export const BeforeAfter = ({ before, after }: BeforeAfterProps) => {
-  const [beforeWidth, setBeforeWidth] = useState(50);
-
-  const [wrapperSize, setWrapperSize] = useState({ height: 0, width: 0 });
+export const BeforeAfter = ({ after, before }: BeforeAfterProps) => {
+  const [wrapperSize, setWrapperSize] = useState({ width: 0, height: 0 });
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [beforeWidth, setBeforeWidth] = useState(50);
 
   const setSize = () => {
     const width = wrapperRef.current?.clientWidth || 0;
     const height = wrapperRef.current?.clientHeight || 0;
-    setWrapperSize({ height, width });
+    setWrapperSize({ width, height });
   };
 
   useEffect(() => {
@@ -29,9 +27,10 @@ export const BeforeAfter = ({ before, after }: BeforeAfterProps) => {
 
   return (
     <div
-      className="relative h-full w-full border-[1.5rem] border-purple-900 rounded-xl overflow-hidden"
       ref={wrapperRef}
+      className="relative h-full w-full rounded-xl overflow-hidden border-[1.5rem] border-purple-900"
     >
+      {/* before */}
       <div
         style={{ maxWidth: beforeWidth + "%" }}
         className="absolute inset-0 w-full h-full z-10 overflow-hidden"
@@ -44,19 +43,21 @@ export const BeforeAfter = ({ before, after }: BeforeAfterProps) => {
           {...before}
         />
       </div>
+      {/* after */}
       <div className="absolute inset-0 w-full h-full">
         <img
           width={wrapperSize.width}
           height={wrapperSize.height}
-          style={{ height: wrapperSize.height + "px" }}
           className="object-cover max-w-none"
+          style={{ height: wrapperSize.height + "px" }}
           {...after}
         />
       </div>
 
+      {/* slider button */}
       <div
         style={{ left: beforeWidth + "%" }}
-        className="absolute text-gray-800 bg-gray-50 p-1 rounded-full z-20 top-1/2 -translate-y-1/2 -translate-x-1/2 shadow-lg pointer-events-none"
+        className="absolute text-gray-800 bg-gray-50 p-1 rounded-full z-30 top-1/2 -translate-y-1/2 -translate-x-1/2 shadow-lg pointer-events-none"
       >
         <svg
           stroke="currentColor"
@@ -70,14 +71,18 @@ export const BeforeAfter = ({ before, after }: BeforeAfterProps) => {
           <path d="M377.941 169.941V216H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.568 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296h243.882v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.568 0-33.941l-86.059-86.059c-15.119-15.12-40.971-4.412-40.971 16.97z"></path>
         </svg>
       </div>
+
+      {/* slider */}
       <input
         type="range"
         min={0}
         max={100}
         value={beforeWidth}
-        onChange={(e) => setBeforeWidth(+e.target.value)}
-        style={{ ["--element-height" as any]: wrapperSize.height + "px" }}
-        className="z-10 h-full w-full absolute inset-0 appearance-none bg-purple-900/20 hover:bg-transparent"
+        onChange={(e) => {
+          setBeforeWidth(+e.target.value);
+        }}
+        className="appearance-none absolute z-20 inset-0 bg-purple-900/20 hover:bg-transparent"
+        style={{ ["--wrapper-height" as any]: wrapperSize.height + "px" }}
       />
     </div>
   );
